a7f29069c0db13c4318cfee8381d0ff8b227ca3a
Harshi <harshipulavara0514@gmail.com>
2026-09-25 15:21:54 +0530

    fixed the base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "task-management-backend-production-e618.up.railway.app"
+    baseURL: "https://task-management-backend-production-e618.up.railway.app"
 });
 
 api.interceptors.request.use(
